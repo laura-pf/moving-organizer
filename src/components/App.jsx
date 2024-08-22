c100e3f7c86964ec3f50aa1e633974f0a83b5b47
Laura Parra <laupafer93@gmail.com>
2024-08-22 12:53:57 +0200

    add landing pink

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,15 @@ import "../scss/App.scss";
 
 function App() {
   return (
-    
+    <main className="landing">
+      <h1 className="landing__tittle">Organizador de </h1>
+      <h1 className="landing__tittle">Mudanzas</h1>
+      <div className="landing__button">
+        <a className="landing__button-link" href="">
+          COMENZAR
+        </a>
+      </div>
+    </main>
   );
 }
 
