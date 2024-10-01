b3467bfb47e482c03ad8085c5dc08d117cf46d3b
Laura Parra <laupafer93@gmail.com>
2024-10-01 13:41:09 +0200

    styles: change on box

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -29,66 +29,69 @@ function Box(props) {
       <Link className="containbox__back" to="/main">
         &lt; Volver
       </Link>
-      <h1 className="containbox__tittle">{props.box.tittle}</h1>
-      <form className="formAddObject">
-        <label htmlFor="item" className="formAddObject__tittle">
-          Guardar en la caja:
-        </label>
-        <div className="formAddObject__inputButton">
-          <input
-            className="formAddObject__inputButton-inputObject"
-            value={props.inputObject}
-            type="text"
-            name="item"
-            id="item"
-            onChange={handleObjectInput}
-          />
-          <button
-            className="formAddObject__inputButton-button"
-            type="submit"
-            onClick={handleAddObject}
-          >
-            Añadir
-          </button>
-        </div>
-      </form>
 
-      {props.box.message && (
-        <h3 className="label message">{props.box.message}</h3>
-      )}
-
-      <ul className="object-list">
-        {props.box.objects.map((object, index) => (
-          <li className="object-list__item" key={index}>
-            <div className="object-list__check-item">
-              <input
-                className="check"
-                checked={object.checked}
-                onChange={() => handleCheckboxChange(index)}
-                type="checkbox"
-              />
-
-              <span
-                style={{
-                  textDecoration: object.checked ? "line-through" : "none",
-                }}
-              >
-                {object.text}
-              </span>
-            </div>
-            <span
-              onClick={() => handleClickRemoveItem(index)}
-              className="close item-close"
+      <div>
+        <h1 className="containbox__tittle">{props.box.tittle}</h1>
+        <form className="formAddObject">
+          <label htmlFor="item" className="formAddObject__tittle">
+            Guardar en la caja:
+          </label>
+          <div className="formAddObject__inputButton">
+            <input
+              className="formAddObject__inputButton-inputObject"
+              value={props.inputObject}
+              type="text"
+              name="item"
+              id="item"
+              onChange={handleObjectInput}
+            />
+            <button
+              className="formAddObject__inputButton-button"
+              type="submit"
+              onClick={handleAddObject}
             >
-              x
-            </span>
-          </li>
-        ))}
-      </ul>
+              Añadir
+            </button>
+          </div>
+        </form>
 
-      {props.mobileMenuHeader && (
-        <MobileMenuHeader onClickCloseMenu={handleClick} />
-      )}
+        {props.box.message && (
+          <h3 className="label message">{props.box.message}</h3>
+        )}
+
+        <ul className="object-list">
+          {props.box.objects.map((object, index) => (
+            <li className="object-list__item" key={index}>
+              <div className="object-list__check-item">
+                <input
+                  className="check"
+                  checked={object.checked}
+                  onChange={() => handleCheckboxChange(index)}
+                  type="checkbox"
+                />
+
+                <span
+                  style={{
+                    textDecoration: object.checked ? "line-through" : "none",
+                  }}
+                >
+                  {object.text}
+                </span>
+              </div>
+              <span
+                onClick={() => handleClickRemoveItem(index)}
+                className="close item-close"
+              >
+                x
+              </span>
+            </li>
+          ))}
+        </ul>
+
+        {props.mobileMenuHeader && (
+          <MobileMenuHeader onClickCloseMenu={handleClick} />
+        )}
+      </div>
     </div>
   );
 }
